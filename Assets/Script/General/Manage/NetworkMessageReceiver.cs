0068800b42869084c8e97e29f79e7bb3f6a7fe2a
labyrenth <ak5690@naver.com>
2017-05-08 17:48:47 +0900

    5/8 작업
    민자하고 또프레소에서 작업함
    1. FireBase로 데이터 보내는 코드 작성.
    2. FireBase에서 데이터 받는 코드 작성중.

---
+++ Assets/Script/General/Manage/NetworkMessageReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+public class NetworkMessageReceiver : MonoBehaviour {
+    DatabaseReference logReference;
+    public List<string> LogList;
+    NetworkMessageReceiver()
+    {
+
+        logReference = FirebaseDatabase.DefaultInstance.GetReference("GameSessionComments");
+
+        logReference.ChildAdded += HandleChildAdded;
+    }
+
+
+    void HandleChildAdded(object sender, ChildChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+
+        // Do something with the data in args.Snapshot
+    }
+
+   
+}
--- Assets/Script/General/Manage/NetworkMessageSender.cs
+++ Assets/Script/General/Manage/NetworkMessageSender.cs
@@ -1,38 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
 using ClientSide;
 
 public class NetworkMessageSender : MonoBehaviour
 {
+    FirebaseDatabase DB;
+    DatabaseReference DBR;
+
+    DatabaseReference UserReference;
+    DatabaseReference MatchReference;
+    private string targetMessage;
+    public NetworkMessageSender()
+    {
+        DB = FirebaseDatabase.DefaultInstance;
+        DBR = DB.RootReference;
+    }
+    public void SetMatchReference(string MatchID)
+    {
+        MatchReference = DBR.Child("Match").Child(MatchID);
+    }
+
+    private void PushLogToDatabase(string log)
+    {
+        MatchReference.Child("Log").Push().SetValueAsync("log");
+    }
+
+    private void SendAndPush(string message)
+    {
+        Network_Client.Send(message);
+        PushLogToDatabase(message);
+    }
+    
     public void SendSkillVectorToServer(int playerNum,int skillIndex,Vector3 targetVector, float messageSendTime)
     {
-        Network_Client.Send("Skill/" + playerNum + "," + skillIndex + "," + targetVector.x + "," + targetVector.y + "," + targetVector.z + "," + messageSendTime);
+        targetMessage = "Skill/" + playerNum + "," + skillIndex + "," + targetVector.x + "," + targetVector.y + "," + targetVector.z + "," + messageSendTime;
+        SendAndPush(targetMessage);
     }
 
     public void SendPlayerEnemyPositionToServer(Vector3 playerPosition, Vector3 enemyPosition, float messageSendTime)
     {
-        Network_Client.Send("Position/" + playerPosition + "," + enemyPosition + "," + messageSendTime);
+        targetMessage = "Position/" + playerPosition + "," + enemyPosition + "," + messageSendTime;
+        SendAndPush(targetMessage);
     }
 
     public void SendReadyToServer(int playerNum)
     {
-        Network_Client.Send("Ready/" + playerNum);
+        targetMessage = "Ready/" + playerNum;
+        SendAndPush(targetMessage);
     }
 
     public void SendStartedToServer()
     {
-        Network_Client.Send("started");
+        targetMessage = "started";
+        SendAndPush(targetMessage);
     }
 
     public void SendGameOverToServer(int playerNum, float messageSendTime)
     {
-        Network_Client.Send("GameOver/" + playerNum + "," + messageSendTime);
+        targetMessage = "GameOver/" + playerNum + "," + messageSendTime;
+        SendAndPush(targetMessage);
     }
 
     public void SendPlayerStateToServer(int playerNum,int playerStateNum, float messageSendTime)
     {
-        Network_Client.Send("Shepherd/" + playerNum + "," + playerStateNum + "," + messageSendTime);
+        targetMessage = ("Shepherd/" + playerNum + "," + playerStateNum + "," + messageSendTime);
+        SendAndPush(targetMessage);
     }
 
 }
