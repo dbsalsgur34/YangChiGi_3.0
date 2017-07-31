f687a7e24171afecc905a0320ef83d54f284c919
labyrenth <ak5690@naver.com>
2017-07-31 19:15:46 +0900

    7/31 추가수정
    Setting에서 특정 버튼 누를시 효과음이 두번 출력되는 문제 일단 해결함.

--- Assets/Script/Setting/SettingManager.cs
+++ Assets/Script/Setting/SettingManager.cs
@@ -10,9 +10,8 @@ public class SettingManager : ManagerBase {
     private Slider effectSound;
     private Text effectAmount;
 
-    private Button resetButton;
-    private Button ShowDev;
-    private GameObject Warning;
+    public Button resetButton;
+
     private int quality;
 
     // Use this for initialization
@@ -23,9 +22,6 @@ public class SettingManager : ManagerBase {
         effectSound = GameObject.Find("EffectSlider").GetComponent<Slider>();
         effectAmount = GameObject.Find("ShowEffectSound").GetComponent<Text>();
 
-        Warning = GameObject.Find("Warning");
-        resetButton = GameObject.Find("Yes").GetComponent<Button>();
-        this.Warning.SetActive(false);
         this.sound.value = PlayManage.Instance.Sound;
         this.effectSound.value = PlayManage.Instance.EffectSound;
         resetButton.onClick.AddListener(DeleteAllData);
@@ -50,25 +46,10 @@ public class SettingManager : ManagerBase {
         effectAmount.text = effectSound.value.ToString("N0");
     }
 
-    private void ActiveObject(GameObject target)
-    {
-        target.SetActive(true);
-        AudioManager.Instance.PlayOneShotEffectClipByName("Button_Lobby");
-        Debug.Log("Active");
-    }
-
-    private void UnActiveObject(GameObject target)
-    {
-        target.SetActive(false);
-        AudioManager.Instance.PlayOneShotEffectClipByName("Button_Lobby");
-        Debug.Log("DeActive");
-    }
-
     private void DeleteAllData()
     {
         PlayManage.Instance.SendMessage("ResetData");
         this.sound.value = 50;
         this.effectSound.value = 50;
-        this.Warning.SetActive(false);
     }
 }
